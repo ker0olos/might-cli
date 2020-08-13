28f4fdfe3a213949938ae95966be903218310964
Kerolos Zaki <52022280+ItsKerolos@users.noreply.github.com>
2020-08-13 12:39:04 +0200

    added a disabled fix for the sticky elements issue on full page screenshots

--- src/screenshot.js
+++ src/screenshot.js
@@ -14,15 +14,28 @@ import { promisify } from 'util';
 */
 const pageDown = async(page) =>
 {
-  const isEnd = await page.evaluate(() =>
-  {
-    window.scrollBy(0, window.innerHeight);
-
-    return window.scrollY >= (document.body.clientHeight - window.innerHeight);
-  });
-
-  return isEnd;
+  await page.evaluate(() => window.scrollBy(0, window.innerHeight));
 };
+
+// /**
+// * @param { import('puppeteer').Page } page
+// */
+// const removeFixedSticky = async(page) =>
+// {
+//   await page.evaluate(() =>
+//   {
+//     const elements = document.querySelectorAll('body *');
+
+//     elements.forEach((el) =>
+//     {
+//       const { display, position } = window.getComputedStyle(el);
+
+//       // if ((position === 'fixed' || position === 'sticky') && display !== 'none')
+//       if ((position === 'fixed' || position === 'sticky') && display !== 'none')
+//         el.parentNode.removeChild(el);
+//     });
+//   });
+// };
 
 /**
 * @param { { page: import('puppeteer').Page, path: string, full: boolean } } options
@@ -41,11 +54,12 @@ export default async(options) =>
   }
 
   // puppeteer appears to have an issue with full-page screenshots
-  // on some web apps including one of ours
+  // on some web apps including one of ours (possibly due to the use of the viewport units)
   // so we're going to be using this workaround of the foreseeable future
 
   const { pagesCount, extraPixels, viewport } = await page.evaluate(() =>
   {
+    // scroll to the beginning of the page
     window.scrollTo(0, 0);
 
     return {
@@ -64,13 +78,24 @@ export default async(options) =>
 
   const images = [];
 
-  for (let index = 0; index < pagesCount; index++)
+  for (let i = 0; i < pagesCount; i++)
   {
     const image = await page.screenshot();
 
+    images.push(image);
+
     await pageDown(page);
 
-    images.push(image);
+    // this has an issue with sticky elements
+    // since this scrolls the page
+    // sticky elements will appear multiple times
+
+    // this is not considered as an issue
+    // however, we do have this fix here
+    // if we ever needed to act on this
+
+    // if (i === 0)
+    //   await removeFixedSticky(page);
   }
 
   // crop last image extra pixels
